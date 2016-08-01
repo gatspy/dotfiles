a26a4b28475a29669aabe0ffb52d1fb220e6f07a
gatspy <gatspy.if@gmail.com>
2016-08-02 05:49:40 +0800

    add theme file, add tmux submodule

--- python/pyenv.zsh
+++ python/pyenv.zsh
@@ -3,4 +3,5 @@ if (( $+commands[pyenv] ))
 then
   export PYENV_ROOT=/usr/local/var/pyenv
   eval "$(pyenv init -)"
+  if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
 fi
