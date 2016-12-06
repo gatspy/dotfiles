0cc64560c86f72fcbfe92d7bbe1852a92ece8048
Gatspy-Golang <Gatspy@Golang-2.local>
2016-12-07 01:57:25 +0800

    use zsh-nvm speed nvm startup

--- node/nvm.zsh
+++ node/nvm.zsh
@@ -2,5 +2,7 @@
 export NVM_DIR=${HOME}/.nvm
 export NVM_NODEJS_ORG_MIRROR=https://npm.taobao.org/dist
 export NVM_IOJS_ORG_MIRROR=https://npm.taobao.org/mirrors/iojs
-alias nvminit='source $(brew --prefix nvm)/nvm.sh'
+# alias nvminit='source $NVM_DIR/nvm.sh'
+# alias time-nvm='time (source "$NVM_DIR/nvm.sh")'
 # source $(brew --prefix nvm)/nvm.sh --no-use
+# source $(brew --prefix nvm)/nvm.sh
