6444f291b1ad62e0345246b46a673f58cbb9b798
Gatspy-Golang <Gatspy@Golang-2.local>
2016-12-15 12:41:35 +0800

    perform zplug config

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,4 +2,6 @@ alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
 
-alias time-zsh='for i in $(seq 1 10); do /usr/bin/time zsh -i -c exit; done'
+alias time-zsh-ten='for i in $(seq 1 10); do /usr/bin/time zsh -i -c exit; done'
+
+alias time-zsh='time zsh -ic exit'
