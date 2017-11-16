94adae3102cc45f7aecf4cf6aa069e514772f641
gatspy <gatspy.if@gmail.com>
2017-11-16 14:44:10 +0800

    major updata
    add pyenv lazy load
    add rbenv lazy load

--- python/pyenv.zsh
+++ python/pyenv.zsh
@@ -1,9 +1,22 @@
 # init according to man page
 if (( $+commands[pyenv] ))
 then
-  export PYENV_ROOT=/usr/local/var/pyenv
-  eval "$(pyenv init - --no-rehash)"
-  if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
+    export PYENV_ROOT=/usr/local/var/pyenv
+    # eval "$(pyenv init - --no-rehash)"
+    # if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
 fi
 # export PYENV_VIRTUALENV_DISABLE_PROMPT=0
 # export PYTHON_BUILD_MIRROR_URL="http://pyenv.qiniudn.com/pythons/"
+
+# Lazy load pyenv
+if type pyenv &> /dev/null; then
+    local PYENV_SHIMS="${PYENV_ROOT:-${HOME}/.pyenv}/shims"
+    export PATH="${PYENV_SHIMS}:${PATH}"
+    function pyenv() {
+        unset pyenv
+        eval "$(command pyenv init - --no-rehash)"
+        if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
+        # eval "$(command pyenv init -)"
+        pyenv $@
+    }
+fi
--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -2,5 +2,16 @@
 if (( $+commands[rbenv] ))
 then
   export RBENV_ROOT=${HOME}/.rbenv
-  eval "$(rbenv init - --no-rehash)"
+  # eval "$(rbenv init - --no-rehash)"
+fi
+
+
+if type rbenv &> /dev/null; then
+    local RBENV_SHIMS="${RBENV_ROOT:-${HOME}/.rbenv}/shims"
+    export PATH="${RBENV_SHIMS}:${PATH}"
+    function rbenv() {
+        unset rbenv
+        eval "$(command rbenv init - --no-rehash)"
+        rbenv $@
+    }
 fi
