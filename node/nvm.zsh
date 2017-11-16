a6a944ae85e7eb6dac73d26760de2ed6c7cab6b1
gatspy <gatspy.if@gmail.com>
2017-11-16 14:23:13 +0800

    nvm tmux,add dotnet cli complete, disable nvm lazy load

--- node/nvm.zsh
+++ node/nvm.zsh
@@ -3,6 +3,6 @@ export NVM_DIR=${HOME}/.nvm
 export NVM_NODEJS_ORG_MIRROR=https://npm.taobao.org/dist
 export NVM_IOJS_ORG_MIRROR=https://npm.taobao.org/mirrors/iojs
 # alias nvminit='source $NVM_DIR/nvm.sh'
-# alias time-nvm='time (source "$NVM_DIR/nvm.sh")'
+alias time-nvm='time (source "$NVM_DIR/nvm.sh")'
 # source $(brew --prefix nvm)/nvm.sh --no-use
 # source $(brew --prefix nvm)/nvm.sh
