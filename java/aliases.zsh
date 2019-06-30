af80cadc3a2e5b4a8385925ce526d750ff02e08d
gatspy <gatspy.if@gmail.com>
2019-07-01 02:32:47 +0800

    :sparkles: mvn别名和android path

--- java/aliases.zsh
+++ java/aliases.zsh
@@ -2,6 +2,8 @@
 
 alias mvnci='mvn clean install'
 alias mvnc='mvn clean'
+alias mvncp='mvn clean package'
+alias mvncp-st='mvn clean package -DskipTests=true'
 alias mvnt='mvn test'
 alias mvnag='mvn archetype:generate'
 alias mvnnew='mvn archetype:generate -DarchetypeArtifactId=maven-archetype-quickstart'
